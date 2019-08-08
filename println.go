ef2424873ba4fd753bd452c12837d590507d870b
ytu-hakan-aksoy <hakan.aksoy@ytu-hakan-aksoys-MacBook-Pro.local>
2019-08-08 09:01:36 +0300

    print

---
+++ println.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Print Example
+	fmt.Println("hello world")
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
