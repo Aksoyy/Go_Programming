89d76ad6095dc4eb11176c568110cde64bbdd18a
ytu-hakan-aksoy <hakan.aksoy@ytu-hakan-aksoys-MacBook-Pro.local>
2019-08-29 12:14:36 +0300

    channel

---
+++ Channels/channel_closing.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+}
+
+// sent job 1
+// received job 1
+// received job 2
+// sent job 2
+// sent job 3
+// sent all jobs
+// received job 3
+// received all jobs
