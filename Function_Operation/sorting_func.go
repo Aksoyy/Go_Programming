b5f2ff9d46a37af70379b6a57ae64af0a46b90a8
ytu-hakan-aksoy <hakan.aksoy@ytu-hakan-aksoys-MacBook-Pro.local>
2019-09-02 10:23:29 +0300

    sorting

---
+++ Function_Operation/sorting_func.go
@@ -0,0 +1,24 @@
+package main
+
+import "sort"
+import "fmt"
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func main() {
+	fruits := []string{"peachhhhhhh", "banana", "kiwiiii"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
+
+// [banana kiwiiii peachhhhhhh]
