b1739063e5cd72345f3f2faa389161157d8e84ba
aksoyy <hkn.aksoyy@gmail.com>
2019-09-11 14:36:21 +0300

    composition

---
+++ Operations/Composition.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Hesap struct{}
+
+func (h *Hesap) HesapBul() float32 {
+	fmt.Println("Test")
+	return 0
+}
+
+func (h *Hesap) HesapOde(miktar float32) bool {
+	fmt.Println("Test2")
+	return true
+}
+
+type KrediHesap struct {
+	Hesap
+}
+
+func main() {
+
+	test := &KrediHesap{}
+	test.HesapBul()    // Test
+	test.HesapOde(400) // Test2
+}
