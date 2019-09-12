9b3652306ddfab33001b4c0e31bc02f1588ef89b
aksoyy <hkn.aksoyy@gmail.com>
2019-09-12 15:19:44 +0300

    app

---
+++ Web_App/second_app/app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// file_path --> git:(master) ✗ go run app.go
